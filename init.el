7a05a2697b31b3879c46390d85ca186173e83d98
Konrad Ergon <konrad.ergon@gmail.com>
2026-04-07 11:16:12 +0200

    fix

--- init.el
+++ init.el
@@ -1,5 +1,3 @@
-;;;; Packages
-
 (require 'package)
 
 (dolist (pkg '(go-mode magit eat))
@@ -147,6 +145,22 @@
 (with-eval-after-load 'claude-code-ide
   (claude-code-ide-emacs-tools-setup))
 
-;;;; GC
+;;;; Custom
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil)
+ '(package-vc-selected-packages
+   '((claude-code-ide :url
+		      "https://github.com/manzaltu/claude-code-ide.el"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 (setq gc-cons-threshold 800000)
