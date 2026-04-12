ea5c9bbff43d1148e2a785bd37f38db7ddd48141
Konrad Ergon <konrad.ergon@gmail.com>
2026-04-12 17:28:41 +0200

    message

--- early-init.el
+++ early-init.el
@@ -7,7 +7,7 @@
 (setq native-comp-async-report-warnings-errors 'silent)
 (setq byte-compile-warnings '(not free-vars unresolved noruntime lexical make-local))
 (setq idle-update-delay 1.0)
-(setq read-process-output-max 1048576)
+(setq read-process-output-max 1000000)
 
 (setq-default bidi-display-reordering 'left-to-right 
               bidi-paragraph-direction 'left-to-right)
@@ -18,6 +18,8 @@
 (setq highlight-nonselected-windows nil)
 (setq fast-but-imprecise-scrolling t)
 (setq inhibit-compacting-font-caches t)
+
+(setq frame-inhibit-implied-resize t) ;; Supposed to hasten startup
 
 (menu-bar-mode -1)
 (tool-bar-mode -1)
